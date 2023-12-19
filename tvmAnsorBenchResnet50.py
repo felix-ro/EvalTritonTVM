0f80d94ce50b5c7b635809cf9db4fa345e3da98b
Felix Rocke <felix.rocke@gmx.de>
2023-12-19 12:04:29 +0000

    refactoring

--- tvmAnsorBenchResnet50.py
+++ tvmAnsorBenchResnet50.py
@@ -1,5 +1,4 @@
 import torch # IMPORT TORCH BEFORE TVM TO AVOID SYMBOL CLASH
-import torchvision
 from torchvision import transforms
 import numpy as np
 import tvm
@@ -7,17 +6,10 @@ from tvm import relay, auto_scheduler
 from PIL import Image
 
 MODEL_NAME = "resnet50"
-TARGET = "llvm" # "cuda"
+TARGET_NAME = "llvm" # "cuda"
 
-def main():
-    model = torch.hub.load('pytorch/vision:v0.10.0', MODEL_NAME, pretrained=True)
-    model.eval()
 
-    # We grab the TorchScripted model via tracing
-    input_shape = [1, 3, 224, 224]
-    input_data = torch.randn(input_shape)
-    scripted_model = torch.jit.trace(model, input_data).eval()
-
+def getImage():
     img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
     img_path = tvm.contrib.download.download_testdata(img_url, "cat.png", module="data")
     img = Image.open(img_path).resize((224, 224))
@@ -31,25 +23,10 @@ def main():
         ]
     )
     img = my_preprocess(img)
-    img = np.expand_dims(img, 0)
+    return np.expand_dims(img, 0)
 
-    input_name = "input0"
-    shape_list = [(input_name, img.shape)]
-    mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
 
-    target= tvm.target.Target(TARGET)
-
-    batch_size = 1
-    layout = "NHWC"
-    dtype = "float32"
-    log_file = "%s-%s-B%d-%s.json" % (MODEL_NAME, layout, batch_size, target.kind.name)
-
-    tasks, task_weights = auto_scheduler.extract_tasks(mod["main"], params, target)
-
-    for idx, task in enumerate(tasks):
-        print("========== Task %d  (workload key: %s) ==========" % (idx, task.workload_key))
-        print(task.compute_dag)
-
+def tuneAnsor(tasks, task_weights, log_file):
     print("Begin tuning...")
     measure_ctx = auto_scheduler.LocalRPCMeasureContext(repeat=1, min_repeat_ms=300, timeout=10)
 
@@ -62,17 +39,60 @@ def main():
 
     tuner.tune(tune_option)
 
+def compile(log_file, mod, target, params):
     # Compile with the history best
     print("Compile...")
     with auto_scheduler.ApplyHistoryBest(log_file):
         with tvm.transform.PassContext(opt_level=3, config={"relay.backend.use_auto_scheduler": True}):
-            lib = relay.build(mod, target=target, params=params)
+            return relay.build(mod, target=target, params=params)
 
+
+def createGraphExecutor(target, lib, input_shape, dtype): 
     # Create graph executor
+    print("Create Graph Executor...")
     dev = tvm.device(str(target), 0)
     module = tvm.contrib.graph_executor.GraphModule(lib["default"](dev))
     data_tvm = tvm.nd.array((np.random.uniform(size=input_shape)).astype(dtype))
     module.set_input("data", data_tvm)
+    return module, dev
+
+
+def main():
+    model = torch.hub.load('pytorch/vision:v0.10.0', MODEL_NAME, pretrained=True)
+    model.eval()
+
+    # We grab the TorchScripted model via tracing
+    input_shape = [1, 3, 224, 224]
+    input_data = torch.randn(input_shape)
+    scripted_model = torch.jit.trace(model, input_data).eval()
+
+    # Creating Relay Graph
+    img = getImage()
+    input_name = "input0"
+    shape_list = [(input_name, img.shape)]
+    mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
+
+    # Selecting target and preparing logging
+    target = tvm.target.Target(TARGET_NAME)
+    batch_size = 1
+    layout = "NHWC"
+    dtype = "float32"
+    log_file = "%s-%s-B%d-%s.json" % (MODEL_NAME, layout, batch_size, target.kind.name)
+
+    # Extract tasks
+    tasks, task_weights = auto_scheduler.extract_tasks(mod["main"], params, target)
+    for idx, task in enumerate(tasks):
+        print("========== Task %d  (workload key: %s) ==========" % (idx, task.workload_key))
+        print(task.compute_dag)
+
+    # tune the model using Ansor
+    tuneAnsor(task, task_weights, log_file)
+
+    # compile model with history best
+    lib = compile(log_file, mod, target, params)
+
+    # create graph executor
+    module, dev = createGraphExecutor(target, lib, input_shape, dtype)
 
     # Evaluate
     print("Evaluate inference time cost...")
