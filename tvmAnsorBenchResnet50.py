27d9eedf43aa7f24efd4a9086ca136dbb0777f3f
Felix Rocke <felix.rocke@gmx.de>
2023-12-19 15:22:33 +0000

    refactored code

--- tvmAnsorBenchResnet50.py
+++ tvmAnsorBenchResnet50.py
@@ -1,29 +1,12 @@
 import torch # IMPORT TORCH BEFORE TVM TO AVOID SYMBOL CLASH
-from torchvision import transforms
 import numpy as np
 import tvm
 from tvm import relay, auto_scheduler
-from PIL import Image
+
+from utils import getImage
 
 MODEL_NAME = "resnet50"
 TARGET_NAME = "cuda" # "llvm"
-
-
-def getImage():
-    img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
-    img_path = tvm.contrib.download.download_testdata(img_url, "cat.png", module="data")
-    img = Image.open(img_path).resize((224, 224))
-
-    my_preprocess = transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-    img = my_preprocess(img)
-    return np.expand_dims(img, 0)
 
 
 def tuneAnsor(tasks, task_weights, log_file):
