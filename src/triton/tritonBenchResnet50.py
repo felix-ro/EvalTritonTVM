279463eeeed3c82639e7141cbe3894cd94fe6790
Felix <felix.rocke@gmx.de>
2023-12-20 22:35:05 +0000

    refactoring

--- src/triton/tritonBenchResnet50.py
+++ src/triton/tritonBenchResnet50.py
@@ -1,9 +1,9 @@
 import os
-from timeit import Timer
+import urllib
 import torch
 import torch._dynamo
 import torch._inductor.config
-import urllib
+from timeit import Timer
 from PIL import Image
 from torchvision import transforms
 
@@ -12,9 +12,10 @@ NUM_ITERS = 100
 PATH = "Results/Triton/"
 MODEL_NAME = "resnet50"
 
-def timing(opt_model, model, device, input_batch, reps, iters):
-    fileName = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-optimized-" + str(device) + ".txt"
-    f = open(fileName, "w")
+
+def time_CPU(opt_model, model, device, input_batch, reps, iters):
+    file_name = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-optimized-" + str(device) + ".txt"
+    f = open(file_name, "w")
     results = ""
     print("Optimized model: ")
     for i in range(reps):
@@ -23,8 +24,8 @@ def timing(opt_model, model, device, input_batch, reps, iters):
         results += str(t.timeit(number=iters)/iters) + "\n"
     f.write(results)
 
-    fileName = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-unoptimized-" + str(device) + ".txt"
-    f = open(fileName, "w")
+    file_name = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-unoptimized-" + str(device) + ".txt"
+    f = open(file_name, "w")
     results = ""
     print("Unoptimized model: ")
     for i in range(reps):
@@ -35,23 +36,24 @@ def timing(opt_model, model, device, input_batch, reps, iters):
 
 
 def profiler(opt_model, model, device, input_batch, useCuda):
-    fileName = PATH + MODEL_NAME + "/" + "profile-" + MODEL_NAME + "-optimized-" + str(device) + ".txt"
-    f = open(fileName, "w")
+    file_name = PATH + MODEL_NAME + "/" + "profile-" + MODEL_NAME + "-unoptimized-" + str(device) + ".txt"
+    f = open(file_name, "w")
     with torch.autograd.profiler.profile(use_cuda=useCuda) as prof:
-        opt_model(input_batch.to(device))
+        model(input_batch.to(device))
 
     f.write(prof.key_averages().table(sort_by="self_cpu_time_total", top_level_events_only= False))
     print(prof.key_averages().table(sort_by="self_cpu_time_total", top_level_events_only= False))
 
-    fileName = PATH + MODEL_NAME + "/" + "profile-" + MODEL_NAME + "-unoptimized-" + str(device) + ".txt"
-    f = open(fileName, "w")
+    file_name = PATH + MODEL_NAME + "/" + "profile-" + MODEL_NAME + "-optimized-" + str(device) + ".txt"
+    f = open(file_name, "w")
     with torch.autograd.profiler.profile(use_cuda=useCuda) as prof:
-        model(input_batch.to(device))
+        opt_model(input_batch.to(device))
 
     f.write(prof.key_averages().table(sort_by="self_cpu_time_total", top_level_events_only=False))
     print(prof.key_averages().table(sort_by="self_cpu_time_total", top_level_events_only=False))
 
-def measureCuda(model, device, input_batch, reps, iters):
+
+def measure_cuda(model, device, input_batch, reps, iters):
     start = torch.cuda.Event(enable_timing=True)
     end = torch.cuda.Event(enable_timing=True)
     
@@ -73,21 +75,42 @@ def measureCuda(model, device, input_batch, reps, iters):
     
     return model_results
 
-def timingCuda(opt_model, model, device, input_batch, reps, iters):
-    model_results = measureCuda(model, device, input_batch, reps, iters)
-    opt_model_results = measureCuda(opt_model, device, input_batch, reps, iters)
 
-    fileName = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-optimized-" + str(device) + ".txt"
-    f = open(fileName, "w")
+def time_cuda(opt_model, model, device, input_batch, reps, iters):
+    print("Unoptimized Model:")
+    model_results = measure_cuda(model, device, input_batch, reps, iters)
+    print("\nOptimized Model")
+    opt_model_results = measure_cuda(opt_model, device, input_batch, reps, iters)
+
+    file_name = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-optimized-" + str(device) + ".txt"
+    f = open(file_name, "w")
     for res in opt_model_results: 
       f.write(str(res) + "\n")
     f.close()
 
-    fileName = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-unoptimized-" + str(device) + ".txt"
-    f = open(fileName, "w")
+    file_name = PATH + MODEL_NAME + "/" + "results-" + MODEL_NAME + "-unoptimized-" + str(device) + ".txt"
+    f = open(file_name, "w")
     for res in model_results: 
       f.write(str(res) + "\n")
     f.close()
+
+
+def get_test_image():
+    url, file_name = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+    try: urllib.URLopener().retrieve(url, file_name)
+    except: urllib.request.urlretrieve(url, file_name)
+
+    input_image = Image.open(file_name)
+    preprocess = transforms.Compose([
+      transforms.Resize(256),
+      transforms.CenterCrop(224),
+      transforms.ToTensor(),
+      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    return input_batch
 
 
 def main():
@@ -103,7 +126,7 @@ def main():
 
     # Cuda capabilities >= 7.0 needed
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    useCuda = torch.cuda.is_available()
+    use_cuda = torch.cuda.is_available()
     # useCuda = False
     # device = "cpu"
 
@@ -111,40 +134,24 @@ def main():
     model = torch.hub.load('pytorch/vision:v0.10.0', MODEL_NAME, pretrained=True)
     model.to(device)
 
-    url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
-    try: urllib.URLopener().retrieve(url, filename)
-    except: urllib.request.urlretrieve(url, filename)
-
-
-    input_image = Image.open(filename)
-    preprocess = transforms.Compose([
-      transforms.Resize(256),
-      transforms.CenterCrop(224),
-      transforms.ToTensor(),
-      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    input_tensor = preprocess(input_image)
-    input_batch = input_tensor.unsqueeze(0)
-    
+    input_batch = get_test_image()
 
     # Compile the model using inductor backend
     opt_model = torch.compile(model, backend="inductor")
     # Forces compilation bevore measuring
     opt_model(input_batch.to(device))
 
-
     if (device == "cpu"):
         # Measure CPU time using timeit
-        timing(opt_model=opt_model, model=model, device=device, input_batch=input_batch, reps=NUM_REPS, iters=NUM_ITERS)
+        time_CPU(opt_model=opt_model, model=model, device=device, input_batch=input_batch, reps=NUM_REPS, iters=NUM_ITERS)
     else:
-        timingCuda(opt_model=opt_model, model=model, device=device, input_batch=input_batch, reps=NUM_REPS, iters=NUM_ITERS)
-
+        time_cuda(opt_model=opt_model, model=model, device=device, input_batch=input_batch, reps=NUM_REPS, iters=NUM_ITERS)
 
     # Profile execution
-    profiler(opt_model=opt_model, model=model, device=device, input_batch=input_batch, useCuda=useCuda)
+    profiler(opt_model=opt_model, model=model, device=device, input_batch=input_batch, useCuda=use_cuda)
 
-    #os.environ["TORCH_COMPILE_DEBUG"] = "0"
-    #torch._inductor.config.debug = False
+    os.environ["TORCH_COMPILE_DEBUG"] = "0"
+    torch._inductor.config.debug = False
 
 
 if __name__ == "__main__":
