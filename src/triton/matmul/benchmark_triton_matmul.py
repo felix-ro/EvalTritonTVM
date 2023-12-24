60dfcd6fc18cc8c8b9a4d9b257ed97bd509f2e5b
Felix <felix.rocke@gmx.de>
2023-12-24 08:32:42 +0100

    fixed some scripts and started implementing different search space sizes

--- src/triton/matmul/benchmark_triton_matmul.py
+++ src/triton/matmul/benchmark_triton_matmul.py
@@ -28,7 +28,7 @@ def test():
     torch.manual_seed(0)
     a = torch.randn((512, 512), device='cuda', dtype=torch.float16)
     b = torch.randn((512, 512), device='cuda', dtype=torch.float16)
-    triton_output = matmul_tuned(a, b)
+    triton_output = matmul_untuned(a, b)
     torch_output = torch.matmul(a, b)
     print(f"triton_output={triton_output}")
     print(f"torch_output={torch_output}")
@@ -48,11 +48,12 @@ def perf(M, N, K, ms):
         x_vals=[128 * i for i in range(2, 33)],  # Different possible values for `x_name`
         line_arg='provider',  # Argument name whose value corresponds to a different line in the plot
         # Possible values for `line_arg`
-        line_vals=['cublas', 'triton-tuned-default', 'triton-tuned-advanced', 'triton-untuned'],
+        line_vals=['cublas', 'triton-tuned-default', 'triton-tuned-advanced', "triton-tuned-max", 'triton-untuned'],
         # Label name for the lines
-        line_names=["cuBLAS", "Triton default Tuning", "Triton advanced Tuning", "Triton No Tuning"],
+        line_names=["cuBLAS", "Triton Default Tuning", "Triton Advanced Tuning", "Triton Max Tuning",
+                    "Triton No Tuning"],
         # Line styles
-        styles=[('green', '-'), ('blue', '-'), ('orange', '-'), ('grey', '-')],
+        styles=[('green', '-'), ('blue', '-'), ('orange', '-'), ('grey', '-'), ('red', '-')],
         ylabel="TFLOPS",  # Label name for the y-axis
         plot_name="matmul-performance",  # Name for the plot, used also as a file name for saving the plot.
         args={},
@@ -68,6 +69,9 @@ def benchmark(M, N, K, provider):
                                                      quantiles=quantiles)
     if provider == 'triton-tuned-advanced':
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: matmul_tuned(a, b, tuning_level="advanced"),
+                                                     quantiles=quantiles)
+    if provider == "triton-tuned-max": 
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: matmul_tuned(a, b, tuning_level="max"),
                                                      quantiles=quantiles)
     if provider == 'triton-untuned':
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: matmul_untuned(a, b), quantiles=quantiles)
--- src/triton/matmul/tuning_search_space.py
+++ src/triton/matmul/tuning_search_space.py
@@ -1,10 +1,50 @@
 import triton
+import torch
+import random
+
+
+def _get_num_stages_upper_limit():
+    cuda_capabilities = torch.cuda.get_device_capability()
+
+    if cuda_capabilities[0] >= 8:
+        # A100
+        return 5
+    else:
+        # GPUs (P100, V100, T4) with Cuda compute capabilites below 8.0 only support a max of two stages
+        return 2
 
 
 def get_default_tune_params():
     configs = [
-        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=3,
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=1,
                       num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=2,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=2,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=2,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=2,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=2,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=2,
+                      num_warps=2),
+        triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=2,
+                      num_warps=2),
+    ]
+    return configs
+
+
+def get_advanced_tune_params():
+    configs = [
+        # basic configs for compute-bound matmuls
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=3,       
+                      num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
         triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
                       num_warps=4),
         triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
@@ -17,35 +57,25 @@ def get_default_tune_params():
                       num_warps=4),
         triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=5,
                       num_warps=2),
-        triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=5,
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=3,
+                      num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=5,
                       num_warps=2),
-    ]
-    return configs
-
-
-def get_advanced_tune_params():
-    configs = [
-        # basic configs for compute-bound matmuls
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 256, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=3, num_warps=8),
-        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 128, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=3, num_warps=8),
-        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 64, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 256, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 128, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 64, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 128, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 32, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 32, 'BLOCK_K': 32, 'SPLIT_K': 1}, num_stages=5, num_warps=2),
-        # good for int8
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 256, 'BLOCK_K': 128, 'SPLIT_K': 1}, num_stages=3, num_warps=8),
-        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 128, 'BLOCK_K': 128, 'SPLIT_K': 1}, num_stages=3, num_warps=8),
-        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 64, 'BLOCK_K': 128, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 256, 'BLOCK_K': 128, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 128, 'BLOCK_K': 128, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 64, 'BLOCK_K': 64, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 128, 'BLOCK_K': 64, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 32, 'BLOCK_K': 64, 'SPLIT_K': 1}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64, 'BLOCK_N': 32, 'BLOCK_K': 64, 'SPLIT_K': 1}, num_stages=5, num_warps=2),
-    ] + get_configs_io_bound(),
+    ]  # + get_configs_io_bound()
     return configs
 
 
@@ -61,11 +91,48 @@ def get_configs_io_bound():
                 for block_n in [32, 64, 128, 256]:
                     num_warps = 2 if block_n <= 64 else 4
                     configs.append(
-                        triton.Config({'BLOCK_M': block_m, 'BLOCK_N': block_n, 'BLOCK_K': block_k, 'SPLIT_K': 1},
+                        triton.Config({'BLOCK_SIZE_M': block_m, 'BLOCK_SIZE_N': block_n, 'BLOCK_SIZE_K': block_k,
+                                       'GROUP_SIZE_M': 8},
                                       num_stages=num_stages, num_warps=num_warps))
                     # split_k
-                    for split_k in [2, 4, 8, 16]:
-                        configs.append(triton.Config({'BLOCK_M': block_m, 'BLOCK_N': block_n, 'BLOCK_K': block_k,
-                                                      'SPLIT_K': split_k}, num_stages=num_stages, num_warps=num_warps,
-                                                     pre_hook=init_to_zero('C')))
+                    for group_size_m in [2, 4, 8, 16]:
+                        configs.append(triton.Config({'BLOCK_SIZE_M': block_m, 'BLOCK_SIZE_N': block_n,
+                                                      'BLOCK_SIZE_K': block_k, 'GROUP_SIZE_M': group_size_m},
+                                                     num_stages=num_stages, num_warps=num_warps,))
+    return configs
+
+
+def get_search_space(start, end, step):
+    space = []
+    i = start
+    while (i <= end):
+        space.append(i)
+        i += step
+    return space
+
+
+def get_random_tune_params():
+    configs = []
+    i = 0
+
+    block_m_n_space = get_search_space(8, 128, 8)
+    block_k_space = get_search_space(8, 64, 4)
+    group_m_space = get_search_space(2, 8, 1)
+    num_stages_space = get_search_space(1, 2, 1)  # change this to detect max
+    print(num_stages_space)
+    num_warps_space = get_search_space(2, 8, 2)
+
+    while (i < 200):
+        i += 1
+        block_m = block_m_n_space[random.randrange(0, len(block_m_n_space) - 1)]
+        block_n = block_m_n_space[random.randrange(0, len(block_m_n_space) - 1)]
+        block_k = block_k_space[random.randrange(0, len(block_k_space) - 1)]
+        group_m = group_m_space[random.randrange(0, len(group_m_space) - 1)]
+        num_stages = num_stages_space[random.randrange(0, len(num_stages_space) - 1)]
+        num_warps = num_warps_space[random.randrange(0, len(num_warps_space) - 1)]
+
+        configs.append(triton.Config({'BLOCK_M': block_m, 'BLOCK_N': block_n, 'BLOCK_K': block_k,
+                                      'GROUP_SIZE_M': group_m},
+                                     num_stages=num_stages, num_warps=num_warps))
+
     return configs
